97219096917628451e9fd8ddb4d0423b60e0d668
Yamini6302 <129651557+Yamini6302@users.noreply.github.com>
2024-10-28 15:30:18 +0530

    Delete author_service directory

--- author_service/src/main/java/com/ust/author_service/dao/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ust.author_service.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ust.author_service.model.Author;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
